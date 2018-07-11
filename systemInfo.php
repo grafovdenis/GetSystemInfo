75640fa6f86437cddda1533b6b783f000950050c
grafa <grafov.denis@gmail.com>
2018-07-11 23:16:44 +0300

    unix systemInfo

--- systemInfo.php
+++ systemInfo.php
@@ -20,13 +20,32 @@ try {
     {
         function ram_info()
         {
-            $cmd_total = "wmic computersystem get TotalPhysicalMemory";
-            exec($cmd_total, $total);
-            $total = intval($total[1] / pow(2, 20));
-            $cmd_free = "wmic OS get FreePhysicalMemory /Value";
-            exec($cmd_free, $free);
-            $free = intval(explode('=', $free[2])[1] / pow(2, 10));
-            return [$total, $free];
+            if (explode(" ", php_uname("s"))[0] == "Windows") {
+                $cmd_total = "wmic computersystem get TotalPhysicalMemory";
+                exec($cmd_total, $total);
+                $total = intval($total[1] / pow(2, 20));
+                $cmd_free = "wmic OS get FreePhysicalMemory /Value";
+                exec($cmd_free, $free);
+                $free = intval(explode('=', $free[2])[1] / pow(2, 10));
+                return [$total, $free];
+            } else {
+                $fh = fopen('/proc/meminfo', 'r');
+                $total = 0;
+                $free = 0;
+                while ($line = fgets($fh)) {
+                    $pieces = array();
+                    if (preg_match('/^MemTotal:\s+(\d+)\skB$/', $line, $pieces)) {
+                        $total = intval($pieces[1] / 1024);
+                    }
+                    if (preg_match('/^MemFree:\s+(\d+)\skB$/', $line, $pieces)) {
+                        $free = intval($pieces[1] / 1024);
+                        break;
+                    }
+                }
+                fclose($fh);
+
+                return [$total, $free];
+            }
         }
     }
 
@@ -34,25 +53,29 @@ try {
     {
         function rom_info()
         {
-            //$cmd = "wmic logicaldisk list brief";
-            $cmd = "wmic logicaldisk get deviceid, freespace,size";
-            exec($cmd, $result);
-            for ($i = 0; $i < sizeof($result); $i++) {
-                $result[$i] = preg_replace('/\s+/', ' ', $result[$i]);
-            }
-            echo "<table>";
-            foreach ($result as $str) {
-                echo "<tr>";
-                foreach (explode(" ", $str) as $item) {
-                    if (is_numeric($item)) {
-                        $item /= pow(2, 30);
-                        $item = intval($item) . " ГБ";
-                    } else $item = "<strong>$item</strong>";
-                    echo "<td>" . $item . "</td>";
+            if (explode(" ", php_uname("s"))[0] == "Windows") {
+                $cmd = "wmic logicaldisk get deviceid, freespace,size";
+                exec($cmd, $result);
+                for ($i = 0; $i < sizeof($result); $i++) {
+                    $result[$i] = preg_replace('/\s+/', ' ', $result[$i]);
                 }
-                echo "</tr>";
+                echo "<table>";
+                foreach ($result as $str) {
+                    echo "<tr>";
+                    foreach (explode(" ", $str) as $item) {
+                        if (is_numeric($item)) {
+                            $item /= pow(2, 30);
+                            $item = intval($item) . " ГБ";
+                        } else $item = "<strong>$item</strong>";
+                        echo "<td>" . $item . "</td>";
+                    }
+                    echo "</tr>";
+                }
+                echo "</table>";
+            } else {
+                echo "Всего: " . intval(disk_total_space(".") / pow(2, 30)) . " ГБ" . "<br>";
+                echo "Доступно: " . intval(disk_free_space(".") / pow(2, 30)) . " ГБ";
             }
-            echo "</table>";
         }
     }
 
@@ -82,7 +105,8 @@ try {
             echo (new os)->get_os();
             break;
         case "ram":
-            echo "<strong>Всего оперативной памяти: </strong>" . (new ram)->ram_info()[0] . " МБ" . "<br>" . "<strong>Доступно оперативной памяти: </strong>" . (new ram)->ram_info()[1] . " МБ";
+            echo "<strong>Всего оперативной памяти: </strong>" . (new ram)->ram_info()[0] . " МБ"
+                . "<br>" . "<strong>Доступно оперативной памяти: </strong>" . (new ram)->ram_info()[1] . " МБ";
             break;
         case "rom":
             echo (new rom)->rom_info();
