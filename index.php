f2a2ec69d61e38524deada8ada4cfb6409a17b8e
Grafov Denis <grafov.denis@gmail.com>
2018-07-24 19:10:49 +0300

    jquery request

--- index.php
+++ index.php
@@ -3,30 +3,44 @@
 <head>
     <title>Text Exercise</title>
     <link rel="stylesheet" type="text/css" href="style.css">
-    <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+    <script
+            src="https://code.jquery.com/jquery-3.3.1.js"
+            integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+            crossorigin="anonymous"></script>
 </head>
 
 <body>
 <div id="info">
-<!--TODO обработка json'а-->
+
 </div>
 <div id="get_info">
-    <button type="button" onclick="getInfo(this.id)" id="full">Get info</button>
+    <button type="button" id="full">Get info</button>
 </div>
 <script>
-    //TODO переписать на jquery
-    function getInfo(id) {
-        let xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState === 4 && this.status === 200) {
-                if (id === "full") {
-                    document.getElementById("info").innerHTML = this.responseText;
-                } else document.getElementById(id).innerHTML = this.responseText;
+    $('#full').click(function () {
+        let id = this.id;
+        $.ajax({
+            url: 'systemInfo.php',
+            data: {"type": id},
+            type: 'get',
+            success: function (result) {
+                if (result['result']) {
+                    if (id === "full") {
+                        console.log(JSON.stringify(result));
+                        $('#info').html('Ваш процессор загружен на ' + result['data']['cpu_usage'] + "%</br>" +
+                            'Ваша OC: ' + result['data']['os_info'] + "</br>" +
+                            '<strong>Оперативная память: </strong>' + "</br>" +
+                            'Всего: ' + Math.round(result['data']['ram_info']['total'] / Math.pow(2, 20)) + " МБ</br>" +
+                            'Доступно: ' + Math.round(result['data']['ram_info']['free'] / Math.pow(2, 10)) + " МБ</br>" +
+                            '<strong>Постоянная память: </strong>' + "</br>" +
+                            '<strong>' + result['data']['rom_info']['device'] + '</strong>' + " Всего: " + Math.round(result['data']['rom_info']['size'] / Math.pow(2, 20)) + ' МБ ' +
+                            "Доступно: " + Math.round(result['data']['rom_info']['free_space'] / Math.pow(2, 20)) + ' МБ'
+                        );
+                    } else $('#' + id).innerHTML = JSON.stringify(result);
+                }
             }
-        };
-        xhttp.open("GET", "systemInfo.php?q=" + id, true);
-        xhttp.send(id)
-    }
+        })
+    });
 </script>
 
 </body>
--- systemInfo.php
+++ systemInfo.php
@@ -1,91 +1,75 @@
 <?php
 header('Content-Type: application/json');
-try {
-    function onError()
+
+class get_info
+{
+    public $os_family = "";
+    public $version = "";
+
+    static function cpu_usage()
     {
-        exit("Something went wrong");
+        $cmd = "wmic cpu get loadpercentage";
+        exec($cmd, $result);
+        $res = array();
+        if ($result != 0) {
+            $res = $result[1];
+        } else onError();
+        return $res;
     }
 
-    class cpu
+
+    static function ram_info()
     {
-        function cpu_usage()
-        {
-            $cmd = "wmic cpu get loadpercentage";
-            exec($cmd, $result);
-            $res = array();
-            if ($result != 0) {
-                $res = array('cpu usage' => $result[1]);
-            } else onError();
-            return $res;
-        }
+        $cmd_total = "wmic computersystem get TotalPhysicalMemory";
+        $cmd_free = "wmic OS get FreePhysicalMemory /Value";
+        exec($cmd_total, $total);
+        exec($cmd_free, $free);
+        $total = $total[1];
+        $free = explode('=', $free[2])[1];
+        return array('total' => $total, 'free' => $free);
     }
 
-    class ram
+    static function rom_info()
     {
-        function ram_info()
-        {
-            $cmd_total = "wmic computersystem get TotalPhysicalMemory";
-            $cmd_free = "wmic OS get FreePhysicalMemory /Value";
-            exec($cmd_total, $total);
-            exec($cmd_free, $free);
-            $total = $total[1];
-            $free = explode('=', $free[2])[1];
-            return array('ram info' => array('total' => $total, 'free' => $free));
+        $cmd = "wmic logicaldisk get deviceid, freespace,size";
+        exec($cmd, $result);
+        $res = array('device' => "", 'free_space' => "", 'size' => "");
+        for ($i = 0; $i < sizeof($result); $i++) {
+            $result[$i] = preg_replace('/\s+/', ' ', $result[$i]);
         }
+        for ($i = 1; $i < sizeof($result) - 1; $i++) {
+            $tmp = explode(" ", $result[1]);
+            $res['device'] = $tmp[0];
+            $res['free_space'] = $tmp[1];
+            $res['size'] = $tmp[2];
+        }
+        return $res;
     }
 
-    class rom
+
+    static function os_info()
     {
-        function rom_info()
-        {
-            $cmd = "wmic logicaldisk get deviceid, freespace,size";
-            exec($cmd, $result);
-            $res =
-                array('rom info' => array('devices' => array(), 'free space' => array(), 'size' => array()));
-            for ($i = 0; $i < sizeof($result); $i++) {
-                $result[$i] = preg_replace('/\s+/', ' ', $result[$i]);
-            }
-            for ($i = 1; $i < sizeof($result) - 1; $i++) {
-                $tmp = explode(" ", $result[1]);
-                array_push($res['rom info']['devices'], $tmp[0]);
-                array_push($res['rom info']['free space'], $tmp[1]);
-                array_push($res['rom info']['size'], $tmp[2]);
-            }
-            return $res;
-        }
+        $os_family = explode(" ", php_uname("s"))[0];
+        $version = php_uname("r");
+        return $os_family . ' ' . $version;
     }
+}
 
-    class os
-    {
-        public $os_family = "";
-        public $version = "";
+function onError()
+{
+    exit("Something went wrong");
+}
 
-        public function __construct()
-        {
-            $this->os_family = explode(" ", php_uname("s"))[0];
-            $this->version = php_uname("r");
-        }
 
-        public function os_info()
-        {
-            return array('os info' =>
-                array('family' => $this->os_family, 'version' => $this->version));
-        }
-    }
-
-    $id = $_GET["q"];
-    switch ($id) {
-        case "full":
-            $res = array('result' => 'true', 'data' => array());
-            array_push($res['data'], (new cpu)->cpu_usage());
-            array_push($res['data'], (new ram)->ram_info());
-            array_push($res['data'], (new rom)->rom_info());
-            array_push($res['data'], (new os)->os_info());
-            echo json_encode($res);
-            break;
-    }
-
-} catch (Exception $e) {
-    echo 'Выброшено исключение: ', $e->getMessage(), "\n";
+$id = $_GET["type"];
+switch ($id) {
+    case "full":
+        $res = array('result' => 'true', 'data' => array());
+        $res['data']['cpu_usage'] = get_info::cpu_usage();
+        $res['data']['ram_info'] = get_info::ram_info();
+        $res['data']['rom_info'] = get_info::rom_info();
+        $res['data']['os_info'] = get_info::os_info();
+        echo json_encode($res);
+        break;
 }
 ?>
